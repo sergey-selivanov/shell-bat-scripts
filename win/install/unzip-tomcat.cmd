a98e18c95b15627b9a832e2661633f35c2ab1715
sergeys <sergeys@zeppelin>
2020-06-10 19:42:48 +0300

    unzip-tomcat

--- win/install/unzip-tomcat.cmd
+++ win/install/unzip-tomcat.cmd
@@ -2,6 +2,12 @@
 
 : unzip-tomcat apache-tomcat.zip 7
 
+if "" == "%1" goto warn
+
+set PORT_BASE=%2
+if "" == "%PORT_BASE%" set PORT_BASE=7
+
+
 set TARGETDIR=c:\binsrv
 
 set file=%1
@@ -13,34 +19,43 @@ set filename=%~n1
 
 echo %filename%
 
-set PORT_SHUTDOWN=%2005
-set PORT_HTTP=%2080
-set PORT_HTTPS=%2443
-set PORT_AJP=%2009
-		
+set PORT_SHUTDOWN=%PORT_BASE%005
+set PORT_HTTP=%PORT_BASE%080
+set PORT_HTTPS=%PORT_BASE%443
+set PORT_AJP=%PORT_BASE%009
+
 echo %PORT_SHUTDOWN%
-		
+
 c:\cygwin\bin\unzip -o %1 -d %TARGETDIR%
 
+set CONF=%TARGETDIR%\%filename%\conf\server.xml
 echo "Editing %CONF% ..."
 
-set CONF=%TARGETDIR%\%filename%\conf\server.xml
-
-: --in-place=.orig 
+: --in-place=.orig
 :move %CONF% %CONF%.orig
 :pause
 
 pushd %TARGETDIR%
 
 c:\cygwin\bin\sed ^
-	--in-place=.orig ^
-    -e s/port="""8005"""/port="""%PORT_SHUTDOWN%"""/ ^
-    -e s/port="""8080"""/port="""%PORT_HTTP%"""/ ^
-    -e s/port="""8443"""/port="""%PORT_HTTPS%"""/ ^
-    -e s/redirectPort="""8443"""/redirectPort="""%PORT_HTTPS%"""/ ^
-    -e s/port="""8009"""/port="""%PORT_AJP%"""/ ^
-    %CONF%
-	
+ --in-place=.orig ^
+ -e s/port="""8005"""/port="""%PORT_SHUTDOWN%"""/ ^
+ -e s/port="""8080"""/port="""%PORT_HTTP%"""/ ^
+ -e s/port="""8443"""/port="""%PORT_HTTPS%"""/ ^
+ -e s/redirectPort="""8443"""/redirectPort="""%PORT_HTTPS%"""/ ^
+ -e s/port="""8009"""/port="""%PORT_AJP%"""/ ^
+ %CONF%
+
 :	> %CONF%
 
 popd
+
+call c:\bin\link-dir.cmd %TARGETDIR%\%filename% %TARGETDIR%\tomcat
+
+goto end
+
+:warn
+
+echo Usage: %~nx0 apache-tomcat-xx.zip [7]
+
+:end
